6f4a5f6bba5d6a943a81f2b1b18be71f2a2c9ef3
VictorCyprien <victor.cyprien@limayrac.fr>
2020-10-24 12:47:52 +0200

    Retour ancienne version dû au PHP et GitHub

--- js/script.js
+++ js/script.js
@@ -1,46 +1,13 @@
 $(function(){
 
   
-    $(".navbar a, footer a").on("click", function(event){
-
-        event.preventDefault();
-        var hash = this.hash;
-
-        $("body, html").animate({scrollTop: $(hash).offset().top}, 900, function(){window.location.hash = hash;})
-
-    });
+  $(".navbar a, footer a").on("click", function(event){
+  
+    event.preventDefault();
+    var hash = this.hash;
     
-    $('#contact-form').submit(function(e){
-        e.preventDefault();
-        $('.comments').empty();
-        var postdata = $('#contact-form').serialize();
-        
-        $.ajax({
-            type: 'POST',
-            url: 'php/contact.php',
-            data: postdata,
-            dataType: 'json',
-            success: function(result){
-                
-                if(result.isSuccess){
-                    
-                    $("#contact-form").append("<p class='thank-you'>Votre message à bien été envoyé. Merci de m'avoir contacté =)</p>");
-                    $("#contact-form")[0].reset();
-                    
-                } else {
-                    
-                    $("#firstname + .comments").html(result.firstnameError);
-                    $("#name + .comments").html(result.nameError);
-                    $("#email + .comments").html(result.emailError);
-                    $("#phone + .comments").html(result.phoneError);
-                    $("#message + .comments").html(result.messageError);
-                    
-                }
-                
-            }
-            
-        });
-        
-    });
+    $("body, html").animate({scrollTop: $(hash).offset().top}, 900, function(){window.location.hash = hash;})
+    
+  })
   
 })
